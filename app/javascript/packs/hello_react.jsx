e487a2c1bbcb9bf0ec6a2e561f0c25faca6f4024
AdemilsonSimiao <biassistencia@gmail.com>
2022-04-05 23:20:07 +0100

    first commit

---
+++ app/javascript/packs/hello_react.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import  PropTypes  from "prop-types";
+
+const Hello = props => (
+    <div>Hello{props.name}!</div>
+)
+Hello.defaultProps ={
+    name: 'bi'
+}
+Hello.propTypes = {
+    name: PropTypes.string
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+    ReactDOM.render(
+        <Hello name="React"/>,
+        document.body.appendChild(document.createElement('div')),
+    )
+})
